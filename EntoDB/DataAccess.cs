07378184ad5ee34a01b400b27da1cda93b6e3705
Rik Wouters <rikwouters@sogyo.nl>
2019-01-07 12:39:14 +0100

    Inserting from the UI forms into the database is now working for GenusName, SpeciesName and CollectionName. I extended the specimen input form with more fields, but none of the new fields are working yet. Some fields, of which the labels are colored in red, are new ideas that are yet to be implemented into the database. These fields are not yet introduced into the SQl DDLs.

--- EntoDB/DataAccess.cs
+++ EntoDB/DataAccess.cs
@@ -8,7 +8,6 @@ namespace Rapento
 {
     class DataAccess
     {
-        //Deze methodes zijn gevoelig voor SQL injectie. Oplossing: stored procedures.
 
         public List<Individual> GetIndividuals()
         {
@@ -19,13 +18,13 @@ namespace Rapento
             }
         }
 
-        public void AddIndividual(string genus, string species, string collection) //Collectie wordt niet aangemaakt in db???
+        public void AddIndividual(string genus, string species, string collection)
         {
             using (IDbConnection connection = new SqlConnection(connectionString: Helper.CnnVal("Rapento.Properties.Settings.Database1ConnectionString")))
             {
                 List<Individual> individual = new List<Individual>();
                 individual.Add(new Individual { GivenGenusName = genus, GivenSpeciesName = species, GivenCollectionName = collection });
-                connection.Execute("dbo.AddIndividual @GivenGenusName, @GivenSpeciesName, @GivenCollectionName", individual);
+                connection.Execute("dbo.AddIndividual @GivenGenusName = @GivenGenusName, @GivenSpeciesName = @GivenSpeciesName, @GivenCollectionName = @GivenCollectionName", individual);
             }
         }
     }
