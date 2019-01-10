17e7320521ce3e9c6f58c6a4c6a5ca2658249ca2
Rik Wouters <rikwouters@sogyo.nl>
2019-01-10 17:44:24 +0100

    Added the Rapento Remote project, to try and send http to the webapi with an interface. While I get a code 200 OK response, I couldn't yet manage to show the correct data in the textblock.

---
+++ RapentoRemote/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Windows;
+
+namespace RapentoRemote
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void FindTaxonIDButton_Click(object sender, RoutedEventArgs e)
+        {
+            string myJson = "{'GivenTaxonName': '" + TaxonNameBox.Text + "'}";
+            using (var client = new HttpClient())
+            {
+                var response = await client.PostAsync("http://localhost:50683/api/Taxons/", 
+                    new StringContent(myJson, Encoding.UTF8, "application/json"));
+                FindTaxonIDResult.Text = response.ToString();
+            }
+        }
+    }
+}
